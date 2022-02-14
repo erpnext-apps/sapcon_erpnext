418e41eb5d3ae42bf493894c47536a8c9dbc2188
Krithi Ramani <krithi.ramani@gmail.com>
2022-02-14 17:58:38 +0530

    Changes while testing in staging instance

--- custom_sapcon/custom_pdf_merge.py
+++ custom_sapcon/custom_pdf_merge.py
@@ -4,56 +4,61 @@ import frappe
 import os
 from frappe import _
 
-import PyPDF2 
+import PyPDF2
 
 @frappe.whitelist()
 def custom_pdf_merge(doctype,docid):
 
-	mergeFile = PyPDF2.PdfFileMerger()	
+	mergeFile = PyPDF2.PdfFileMerger()
 
-	final_merged_file = _("Merged_{}.pdf").format(docid)
+	final_merged_file = _("/private/files/Merged_{}.pdf").format(docid)
 
 	#Generate pdf of original record
+	org_pdf = docid + ".pdf"
 	doc_pdf = frappe.attach_print(doctype, docid,str(docid), print_format="")
-	docfile = open(_("{}.pdf").format(docid),'wb')
+#	docfile = open(_("{}.pdf").format(docid),'w+')
+	docfile = open(org_pdf,"wb")
 	docfile.write(doc_pdf["fcontent"])
-	doc_to_merge = PyPDF2.PdfFileReader(_("{}.pdf").format(docid),'rb')
+	doc_to_merge = PyPDF2.PdfFileReader(org_pdf,'rb')
 	mergeFile.append(doc_to_merge,'rb')
 
-	#Fetch attachments 
+	#Fetch attachments
 	attached_docs = frappe.get_all("File",
-			fields=["name", "file_name", "file_url", "is_private"],
-			filters = {"attached_to_name": docid, "attached_to_doctype": doctype, "is_private":0})
-	
-	file_path = frappe.utils.get_files_path(
-				attached_docs[0].file_name, is_private=0)
-	dir_path_idx = file_path.rfind('/')
-	dir_path =file_path[0:dir_path_idx] + '/'
+			fields=["name", "file_name", "file_url"] ,
+			filters = {"attached_to_name": docid, "attached_to_doctype": doctype,"file_name":["like","%.pdf"]})
+
+	file_path = frappe.utils.get_url()
+
+	dir_path_idx = file_path.find('/')+2
+	dir_path =file_path[dir_path_idx:]
+
+#	print (dir_path)
 
 	#Append all attachments to final pdf
 	for pdfs in attached_docs:
 		if _('Merged_').format(docid) in pdfs.file_name:
 			return dir_path + final_merged_file
-		else: 
-			to_merge =PyPDF2.PdfFileReader(dir_path+pdfs.file_name)
+		else:
+#			print (dir_path + pdfs.file_url)
+			to_merge =PyPDF2.PdfFileReader(dir_path + pdfs.file_url)
 			mergeFile.append(to_merge,'rb')
 
 	#create merged pdf file and add it as attachment to docid
 	if mergeFile:
-		mergeFile.write(dir_path +final_merged_file)
+#		print (dir_path + final_merged_file)
+		mergeFile.write(dir_path + final_merged_file)
 		mergeFile.close()
 		file_stats = os.stat(dir_path + final_merged_file)
 		file_size = file_stats.st_size
-		
+
 		merged_file = frappe.get_doc({
 					"doctype": "File",
-					"file_name":final_merged_file,
-					"file_url": "/files/" + final_merged_file,
+					"file_name": "Merged_"+docid+".pdf",
+					"file_url":final_merged_file,
 					"attached_to_doctype": doctype,
 					"attached_to_name": docid,
 					"file_size":file_size,
-					"is_private":0
 				})
 		merged_file.insert()
-	
+#	frappe.msgprint(dir_path + final_merged_file)
 	return dir_path + final_merged_file
